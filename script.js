0736c4e49a73f6f5b664c9ab84ba0d61f48adfc7
sukhdeepsohal05 <sukhdeepsohal05@gmail.com>
2022-04-22 18:29:27 +0530

    update script.js

--- script.js
+++ script.js
@@ -10,6 +10,21 @@ function playSound(e){
     audio.play();
 }
 
+const keys = document.querySelectorAll('.key');
+keys.forEach(key=>{
+    key.addEventListener('click', ()=>{
+        const audio = document.querySelector(`audio[src='sounds/${key.getAttribute('data-key')}.mp3']`)
+        if (!audio) return;
+        const pressedKey = document.querySelector(`.key[data-key='${key.getAttribute('data-key')}']`);
+        pressedKey.classList.add('playing');
+        setTimeout(() => {
+            pressedKey.classList.remove('playing');
+        }, 200);
+        audio.currentTime = 0;
+        audio.play();
+    })
+})
+
 if (document.querySelector('div').classList.contains('playing')) {
     let activeKey = document.querySelectorAll('.key.playing');
     activeKey.classList.remove('playing');
